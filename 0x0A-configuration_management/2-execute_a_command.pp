e699d94c15e64d6d348a99288f091afe51f0a37d
SK-Yeboah <skyeboah9@gmail.com>
2024-02-23 10:59:06 -1200

    install

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-# Define an exec resource to kill the process named "killmenow"
-exec { 'killmenow':
-  command     => 'pkill killmenow',  # Use pkill to kill the process
-  path        => '/bin:/usr/bin',     # Set the PATH environment variable
-  refreshonly => true,                # Only run when notified
-  subscribe   => File['/path/to/killmenow'], # Ensure to subscribe to the file change
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
