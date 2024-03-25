c064787f107d1fc445e0504e52fa651af2faa85e
SK-Yeboah <skyeboah9@gmail.com>
2024-03-25 09:32:37 -1200

    RESTfulAPi_1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,17 @@
-#!/usr/bin/python3
-"""
-Exports to-do list information for a given employee ID to CSV format.
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
-def export_todo_to_csv(employee_id):
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    todos_response = requests.get(url + "todos", params={"userId": employee_id})
-
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Failed to retrieve data. Please check the employee ID.")
-        return
-
-    user_data = user_response.json()
-    username = user_data.get("username")
-    todo_data = todos_response.json()
-
-    filename = f"{employee_id}.csv"
-    with open(filename, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow(["User ID", "Username", "Task ID", "Task Title", "Completed"])
-
-        for task in todo_data:
-            writer.writerow([employee_id, username, task["id"], task["title"], task["completed"]])
-
-    print(f"Todo list for Employee {username} has been exported to {filename}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-    else:
-        employee_id = sys.argv[1]
-        export_todo_to_csv(employee_id)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
