3bad7583a6b05dcc25f9ba79f1639fb191c5834b
SK-Yeboah <skyeboah9@gmail.com>
2024-06-19 01:37:52 -1200

    form2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,16 +15,8 @@ def number_of_subscribers(subreddit):
     # Sending GET request to Reddit API
     response = requests.get(url, headers=headers)
 
-    # Checking if the response is successful (status code 200)
     if response.status_code == 200:
-        # Parsing JSON response
-        data = response.json()
-        # Checking if the subreddit exists
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            # If the subreddit does not exist or data is missing, return 0
-            return 0
+        return response.json().get("data").get("subscribers")
     else:
-        # If the request was not successful, return 0
         return 0
+    
