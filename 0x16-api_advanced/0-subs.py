ba3a454629957abf8a512f0667d0b1bcd2047dab
SK-Yeboah <skyeboah9@gmail.com>
2024-06-19 01:55:03 -1200

    comma

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,23 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    # URL for Reddit API to get subreddit information
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # Setting custom User-Agent to avoid Too Many Requests error
+    headers = {"User-Agent": "Custom"}
+    # Sending GET request to Reddit API
+    response = requests.get(url, headers=headers)
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    # Checking if the response is successful (status code 200)
+    if response.status_code == 200:
+        # Parsing JSON response
+        data = response.json()
+        # Checking if the subreddit exists
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            # If the subreddit does not exist or data is missing, return 0
+            return 0
     else:
+        # If the request was not successful, return 0
         return 0
