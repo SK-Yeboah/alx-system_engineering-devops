c3c050632dedd1f65caeaa71cc9b57dd210020e2
SK-Yeboah <skyeboah9@gmail.com>
2024-06-19 02:07:35 -1200

    comma2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,23 +8,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    # URL for Reddit API to get subreddit information
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # Setting custom User-Agent to avoid Too Many Requests error
-    headers = {"User-Agent": "Custom"}
-    # Sending GET request to Reddit API
-    response = requests.get(url, headers=headers)
-
-    # Checking if the response is successful (status code 200)
-    if response.status_code == 200:
-        # Parsing JSON response
-        data = response.json()
-        # Checking if the subreddit exists
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            # If the subreddit does not exist or data is missing, return 0
-            return 0
-    else:
-        # If the request was not successful, return 0
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
